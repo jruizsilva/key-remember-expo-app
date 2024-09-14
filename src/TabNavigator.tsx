6c3b770bb108c27e24a60476fe63cc8f507868a8
Jonathan Ruiz Silva <ruizsilvajonathan@gmail.com>
2024-09-14 16:40:51 -0300

    search tab screen added

--- src/TabNavigator.tsx
+++ src/TabNavigator.tsx
@@ -2,10 +2,12 @@ import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { HomeScreen } from './screens/tabScreens/HomeScreen'
 import { ProfileScreen } from './screens/tabScreens/ProfileScreen'
+import SearchScreen from './screens/tabScreens/SearchScreen'
 
 export type TabParamList = {
   Home: undefined
   Profile: undefined
+  Search: undefined
 }
 
 const Tab = createBottomTabNavigator<TabParamList>()
@@ -16,15 +18,14 @@ export function TabNavigation() {
       screenOptions={({ navigation, route }) => ({
         tabBarIcon: ({ color, size, focused }) => {
           let iconName: keyof typeof MaterialCommunityIcons.glyphMap
-          switch (route.name) {
-            case 'Home':
-              iconName = focused ? 'home' : 'home-outline'
-              break
-            case 'Profile':
-              iconName = focused ? 'cog' : 'cog-outline'
-              break
-            default:
-              iconName = 'home'
+          if (route.name === 'Home') {
+            iconName = focused ? 'home' : 'home-outline'
+          } else if (route.name === 'Search') {
+            iconName = focused ? 'magnify' : 'magnify'
+          } else if (route.name === 'Profile') {
+            iconName = focused ? 'account' : 'account-outline'
+          } else {
+            iconName = 'home'
           }
 
           return (
@@ -33,8 +34,27 @@ export function TabNavigation() {
         },
       })}
     >
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Inicio',
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{
+          title: 'Buscar',
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: 'Perfil',
+        }}
+      />
     </Tab.Navigator>
   )
 }
---
+++ src/screens/tabScreens/SearchScreen.tsx
@@ -0,0 +1,18 @@
+import { View, StyleSheet } from 'react-native'
+import { Text } from 'react-native-paper'
+
+export default function SearchScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>SearchScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
